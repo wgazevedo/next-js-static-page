fa2df41cfac3b67d22dd4783f694debc06df1d8f
Wellington Gonçalves de Azevedo <wellington.azevedo@outlook.com>
2020-11-10 22:54:37 -0300

    clean code

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,7 @@
-import { useState } from 'react';
-
 function Home() {
     return (
         <div>
-            <h1>Home g</h1>
-            <Contador />
-        </div>
-    )
-}
-
-function Contador(){
-    
-        const [contador, setContador] = useState(1);
-
-        function adicionarContador() {
-            setContador(contador + 1);
-        }
-    return(
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
+            <h1>Home</h1>
         </div>
     )
 }
